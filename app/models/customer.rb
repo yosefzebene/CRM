7e0fe648fe4d0d38fab2ed55a117c794e8cc6b5d
yosef-ZzZ <yosef.zebene1@gmail.com>
2022-11-25 18:06:46 -0600

    Added nav bar and styling

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
     has_one_attached :picture do |attachable|
-        attachable.variant :profile, resize_to_limit: [100, 100]
+        attachable.variant :profile, resize_to_fill: [128, 128]
     end
 
     validates :firstname, :lastname, presence: true, length: { maximum: 100 }
